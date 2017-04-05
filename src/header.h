cce485291571ef039053427ad08d9082e3b891be
Bobby Yang <122@holbertonschool.com>
2017-04-05 18:14:57 +0000

    updated with linked list struct for making argumemt documentation

--- src/header.h
+++ src/header.h
@@ -1,9 +1,10 @@
 #ifndef SEAHORSE
 #define SEAHORSE
-#include <dirent.h> 
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "base_struct.h"
 void handle_c_file(const char *file_name, const char *dir_name, FILE *rf);
 void handle_h_file(const char *file_name, const char *dir_name, FILE *rf);
 int is_c_file(const char *file_name);
